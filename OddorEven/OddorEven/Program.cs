fac8ce70411942d8746f3417216dc472f30ca8cc
Blake Jensen <30006830@student.toiohomai.ac.nz>
2019-03-13 09:37:42 +1300

    program to tell user if number entered is odd or even

---
+++ OddorEven/OddorEven/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddorEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Time for a game!");
+            Console.WriteLine("Type a number and I'll tell you if it is odd or even.");
+            Console.WriteLine("Enter a number now: ");
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+                Console.WriteLine("The number you typed in is Even.");
+            else
+                Console.WriteLine("The number you typed in is Odd.");
+
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+    }
+}
